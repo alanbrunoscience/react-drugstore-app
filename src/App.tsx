9a28bd8b9b4a9df70e53678128ad771e99243e9a
alanbrunoscience <alanbrunomath@gmail.com>
2025-02-25 11:26:03 -0300

    :sparkles: Implement the CRUD - Categoria

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
-import ListaCategorias from './components/categorias/listaCategorias/ListaCategorias'
+import ListarCategorias from './components/categorias/listarcategorias/ListarCategorias'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import FormCategoria from './components/categorias/formcategoria/FormCategoria'
+import DeletarCategoria from './components/categorias/deletarcategorias/DeletarCategoria'
 
 function App() {
   return (
@@ -16,7 +18,10 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
-            {/* <Route path="/categorias" element={<ListaCategorias />} /> */}
+            <Route path="/categorias" element={<ListarCategorias />} />
+            <Route path="/cadastrarcategoria" element={<FormCategoria />} />
+            <Route path="/editarcategoria/:id" element={<FormCategoria />} />
+            <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
           </Routes>
         </div>
         <Footer />
